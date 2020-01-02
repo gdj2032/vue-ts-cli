67ed261f18af3a027758c5117b1336772e0f8ffc
djgu <djgu@tmindtech.com>
2020-01-02 14:49:03 +0800

    add test table

--- src/containers/Home/index.vue
+++ src/containers/Home/index.vue
@@ -3,11 +3,27 @@
     <div>主页</div>
     <button @click="increment">加1</button>
     <div>{{$store.state.count}}</div>
+    <table border="1">
+      <tr>
+        <th>id</th>
+        <th>UID</th>
+        <th>名称</th>
+        <th>num</th>
+        <th>描述</th>
+      </tr>
+      <tr v-for="item in dataSource" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.userId}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.num}}</td>
+        <td>{{item.description}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import { shopService } from "../../service";
+import { shopService } from "@/service";
 
 export default {
   name: "home",
@@ -15,12 +31,14 @@ export default {
   },
   data() {
     return {
+      dataSource: []
     };
   },
   methods: {
-    getShopList: async () => {
+    async getShopList() {
       const [err, info] = await shopService.ShopList({ id: 1 });
       console.log("TCL: err, info", err, info);
+      this.dataSource = info.data;
     },
     increment() {
       this.$store.commit("increment");
